d60539c616f5933e8f405a94b0828af572b82a0c
keithyin <yinpenghhz@hotmail.com>
2021-04-21 23:19:08 +0800

    search tree

--- src/tree/algo_96.rs
+++ src/tree/algo_96.rs
@@ -1,4 +1,23 @@
 
-pub fn num_trees(n: i32) {
+pub fn num_trees(n: i32) -> i32{
+    if n < 1 {
+        return 0;
+    }
+    return num_trees_core(1, n);
+}
 
+pub fn num_trees_core(begin: i32, end: i32) -> i32 {
+    if begin > end {
+        return 1;
+    }
+
+    let mut i = begin;
+    let mut result = 0;
+    while i <= end {
+        let num_left = num_trees_core(begin, i-1);
+        let num_right = num_trees_core(i+1, end);
+        result += (num_left * num_right);
+        i += 1;
+    }
+    return result;
 }
