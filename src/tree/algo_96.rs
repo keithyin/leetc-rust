7c8c547cce12848daa7a16961ffef9cf7a9be6ae
keithyin <yinpenghhz@hotmail.com>
2021-04-23 00:05:05 +0800

    96

--- src/tree/algo_96.rs
+++ src/tree/algo_96.rs
@@ -1,23 +1,30 @@
+use std::collections::HashMap;
 
 pub fn num_trees(n: i32) -> i32{
     if n < 1 {
         return 0;
     }
-    return num_trees_core(1, n);
+    let mut cache = HashMap::new();
+    return num_trees_core(1, n, &mut cache);
 }
 
-pub fn num_trees_core(begin: i32, end: i32) -> i32 {
-    if begin > end {
+pub fn num_trees_core(begin: i32, end: i32, cache: &mut HashMap<i32, i32>) -> i32 {
+    if begin >= end {
         return 1;
+    }
+
+    if let Some(val) = cache.get(&(end-begin)) {
+        return *val;
     }
 
     let mut i = begin;
     let mut result = 0;
     while i <= end {
-        let num_left = num_trees_core(begin, i-1);
-        let num_right = num_trees_core(i+1, end);
+        let num_left = num_trees_core(begin, i-1, cache);
+        let num_right = num_trees_core(i+1, end, cache);
         result += (num_left * num_right);
         i += 1;
     }
+    cache.insert(end - begin, result);
     return result;
 }
